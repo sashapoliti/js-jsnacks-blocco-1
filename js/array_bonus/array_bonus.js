2059ae21439c25d0903701519c2a52ee97d69b96
sashapoliti <sas.politi@gmail.com>
2024-03-12 16:09:12 +0100

    array_bonus: add second and third case, complete - end

--- js/array_bonus/array_bonus.js
+++ js/array_bonus/array_bonus.js
@@ -1,5 +1,5 @@
-const arrayOne = [1, 2, 3, 4, 5, 6];
-const arrayTwo = [1, 2, 3];
+const arrayOne = [1, 2, 3];
+const arrayTwo = [1, 2, 3, 4, 5, 6];
 let subLength = arrayOne.length - arrayTwo.length;
 
 if (subLength > 0) {
@@ -9,7 +9,12 @@ if (subLength > 0) {
     console.log(arrayOne);
     console.log(arrayTwo);
 } else if (subLength < 0) {
-
+    subLength = Math.abs(subLength);
+    for (let i = 0; i < subLength; i++) {
+        arrayOne.push(getRndInteger(1, 10));
+    };
+    console.log(arrayOne);
+    console.log(arrayTwo);
 } else {
     console.log('Nulla da fare, i due array sono già della stezza lungezza');
 }
