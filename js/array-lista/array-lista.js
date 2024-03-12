cba77b69b799354b0a6387effae50f72cb09f366
sashapoliti <sas.politi@gmail.com>
2024-03-12 14:57:25 +0100

    array.lista: add search for watermelon, complete

--- js/array-lista/array-lista.js
+++ js/array-lista/array-lista.js
@@ -12,4 +12,18 @@ const refrigerator = [
 const table = 'pesca';
 
 refrigerator.push(table);
-console.log(refrigerator);
+//console.log(refrigerator);
+
+let flag = false;
+for (let i = 0; i < refrigerator.length; i++) {
+    const watermelon = 'cocomero';
+
+    if (refrigerator[i] === watermelon) {
+        flag = true;
+    };
+};
+if (flag) {
+    console.log('Trovato! Devo solo preparare il cocktail.');
+} else {
+    console.log('Oh no, devo uscire a comprare il cocomero!');
+}
