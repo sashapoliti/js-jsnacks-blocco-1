b4e88c9964817a21a1aef6940ac576109cdf85db
sashapoliti <sas.politi@gmail.com>
2024-03-12 15:32:18 +0100

    array_tail: bonus complete

--- js/array_tail/array_tail.js
+++ js/array_tail/array_tail.js
@@ -1,12 +1,20 @@
 const arrayNumber = parseInt(prompt("Di quanti elementi vuoi sia composto l'array?"));
+const stampNumber = parseInt(prompt("Quanti elementi dell'array vuoi stampare?"))
 const numbers = [];
+const stamp = arrayNumber - stampNumber;
 
-for (let i = 0; i < arrayNumber; i++) {    
-    const randomNumber = getRndInteger(1, 100);
-    numbers.push(randomNumber);
-};
-console.log(numbers);
+if (stamp >= 0) {
+    for (let i = 0; i < arrayNumber; i++) {    
+        const randomNumber = getRndInteger(1, 100);
+        numbers.push(randomNumber);
+    };
+    console.log(numbers);
 
-for (let i = numbers.length - 1; i >= (numbers.length - 1) - 4; i--) {
-    console.log(numbers[i]);
-};
+
+    for (let i = numbers.length - 1; i >= stamp; i--) {
+        console.log(numbers[i]);
+    };  
+} else {
+    console.log('Il tuo array è troppo corto per stampare questa quantità di elementi, riprova.');
+}
+
