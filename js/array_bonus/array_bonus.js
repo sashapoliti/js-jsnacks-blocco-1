251bb9ca6f061b052ac8fd6ddf088b8f72c72467
sashapoliti <sas.politi@gmail.com>
2024-03-12 15:53:15 +0100

    array_bonus: control arrays length

--- js/array_bonus/array_bonus.js
+++ js/array_bonus/array_bonus.js
@@ -1,3 +1,11 @@
 const arrayOne = [1, 2, 3, 4, 5, 6];
 const arrayTwo = [1, 2, 3];
+let subLength = arrayOne.length - arrayTwo.length;
 
+if (subLength > 0) {
+
+} else if (subLength < 0) {
+
+} else {
+    console.log('Nulla da fare, i due array sono già della stezza lungezza');
+}
